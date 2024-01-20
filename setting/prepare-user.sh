ba7ca091bc281e119c24df205ef3181138544c95
Alejandro Tortolero <ajtortolero@outlook.com>
2024-01-20 01:38:24 -0500

    Updated

--- setting/prepare-user.sh
+++ setting/prepare-user.sh
@@ -10,4 +10,24 @@ groupIAM="GrupoFullAccess"
 while IFS= read -r email
 do
   echo "Usuario: $email"
+
+  # Crear el usuario IAM
+  aws iam create-user --user-name "$email"
+
+  # Agregar el usuario al grupo IAM
+  aws iam add-user-to-group --user-name "$email" --group-name "$groupIAM"
+
+  # Asignar una política que otorgue acceso completo a todos los servicios de AWS al usuario
+  aws iam attach-user-policy --policy-arn arn:aws:iam::aws:policy/AdministratorAccess --user-name "$email"
+
+  # Generar y mostrar las credenciales de acceso del usuario
+  access_key_info=$(aws iam create-access-key --user-name "$email")
+  access_key_id=$(echo "$access_key_info" | jq -r '.AccessKey.AccessKeyId')
+  secret_access_key=$(echo "$access_key_info" | jq -r '.AccessKey.SecretAccessKey')
+
+  echo "Usuario: $email"
+  echo "Access Key ID: $access_key_id"
+  echo "Secret Access Key: $secret_access_key"
+  echo "-----------------------------------"
+
 done < "$file"
